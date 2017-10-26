a62891dbfcd8339597d6e4efa41ac4a0363b6c34
DESKTOP-73RF5IL\rm <763457149@qq.com>
2017-10-26 16:41:53 +0800

    检测报名成功api

--- new/src/api/index.js
+++ new/src/api/index.js
@@ -1,26 +1,18 @@
 import axios from 'axios'
 import Qs from 'qs'
 
-const postData = (name, mobile, company, position, origin, openId) => axios.post('http://a.weixin.hndt.com/user/add',
+const postData = (name, mobile, company, position, origin, openId) => axios.post('http://a.weixin.hndt.com/user/add', {
+    name,
+    mobile,
+    company,
+    position,
+    origin,
+    openId
+})
 
-    // Qs.stringify({
-    //     name,
-    //     mobile,
-    //     company,
-    //     position,
-    //     origin,
-    //     openid
-    // })
-    {
-        name,
-        mobile,
-        company,
-        position,
-        origin,
-        openId
-    }
-)
+const checkOpenId = (openId) => axios.get('http://a.weixin.hndt.com/user/check/?openid=' + openId)
 
 export {
-    postData
+    postData,
+    checkOpenId
 }
